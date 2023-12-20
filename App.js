9ba10b86a23bd4de10b30c381b680da89211fa06
Konan <mastercelis8891@gmail.com>
2023-12-20 07:54:43 -0500

    Beginning of the project

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [goalText, setGoalText] = useState("");
+  const [listGoals, setListGoals] = useState([]);
+  function ajouterGoal() {
+      setListGoals((currentListGoal) => [
+        ...currentListGoal,
+        "test"
+      ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <Button title='Add new goal' onPress={ajouterGoal}/>
+      </View>
+      <View style={styles.listGoals}>
+        {listGoals.map((goal) => <Text key={goal} style={styles.itemGoal}>{goal}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  topCenter: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 50
+  },
+  listGoals: {
+    flex:5
+  },
+  itemGoal: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    padding: 8,
+    color: 'white'
+  }
+
 });
